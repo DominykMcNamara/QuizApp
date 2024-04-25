1d6664154dce0ffb7f135c5ef261dc177c6982e0
DominykMcNamara <dominyksmith@gmail.com>
2024-04-25 00:28:55 -0400

    SVG icons and responsive configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     theme: {
+        'backgroundImage': {
+          'icon-accessibility': "url('assets/images/icon-accessibility.svg')",
+            'icon-correct': "url('assets/images/icon-correct.svg')",
+            'icon-css': "url('assets/images/icon-css.svg')",
+            'icon-error': "url('assets/images/icon-error.svg')",
+            'icon-html': "url('assets/images/icon-html.svg')",
+            'icon-incorrect': "url('assets/images/icon-incorrect.svg')",
+            'icon-js': "url('assets/images/icon-js.svg')",
+            'icon-moon-dark': "url('assets/images/icon-moon-dark.svg')",
+            'icon-moon-light': "url('assets/images/icon-moon-light.svg')",
+            'icon-sun-dark': "url('assets/images/icon-sun-dark.svg')",
+            'icon-sun-light': "url('assets/images/icon-sun-light.svg')",
+            'pattern-background-desktop-dark': "url('assets/images/pattern-background-desktop-dark.svg')",
+            'pattern-background-desktop-light': "url('assets/images/pattern-background-desktop-light.svg')",
+            'pattern-background-mobile-dark': "url('assets/images/pattern-background-mobile-dark.svg')",
+            'pattern-background-mobile-light': "url('assets/images/pattern-background-mobile-light.svg')",
+            'pattern-background-tablet-dark': "url('assets/images/pattern-background-tablet-dark.svg')",
+            'pattern-background-tablet-light': "url('assets/images/pattern-background-tablet-light.svg')",
+        },
+
         extend: {
             fontFamily: {
                 sans: ['Rubik','Inter var', ...defaultTheme.fontFamily.sans],
@@ -37,6 +57,11 @@ module.exports = {
                 }]
 
             },
+            screens: {
+                'desktop': '1440px',
+                'tablet': '768px',
+                'mobile': '375px'
+            }
         },
         colors: {
             'purple': '#A729F5',
