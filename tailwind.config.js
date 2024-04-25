b43a1a0f7a0d9f6d55133c4d9dd31242d02438bd
DominykMcNamara <dominyksmith@gmail.com>
2024-04-25 00:16:02 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,54 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ['Rubik','Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                'display': ['144px', {
+                    lineHeight: '100%',
+                    weight: 'medium',
+                }],
+                'hl-bold': ['64px', {
+                    lineHeight: '100%',
+                    weight: 'medium'
+                }],
+                'hl-regular': ['64px', {
+                    lineHeight: '100%',
+                    weight: 'regular'
+                }],
+                'hm': ['36px', {
+                    lineHeight: '100%',
+                    weight: 'regular'
+                }],
+                'hs': ['28px', {
+                    lineHeight: '100%',
+                    weight: 'regular'
+                }],
+                'bm': ['12px', {
+                    lineHeight: '150%',
+                    weight: 'regular'
+                }],
+                'bs': ['12px', {
+                    lineHeight: '150%',
+                    weight: 'regular-italic'
+                }]
+
             },
         },
+        colors: {
+            'purple': '#A729F5',
+            'dark-navy': '#313E51',
+            'navy': '#3B4D66',
+            'grey-navy': '#626C7F',
+            'light-bluish': '#ABC1E1',
+            'light-grey': '#F4F6FA',
+            'white': '#FFFFFF',
+            'green': '#26D782',
+            'red': '#EE5454',
+        }
+
     },
+
     variants: {
         extend: {
             backgroundColor: ['active'],
