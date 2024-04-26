d9dca37240dbe2735ca5dc70c1cefd7aeb1bee7b
DominykMcNamara <dominyksmith@gmail.com>
2024-04-26 00:46:48 -0400

    SVG icons and responsive configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     theme: {
-        'backgroundImage': {
-          'icon-accessibility': "url('assets/images/icon-accessibility.svg')",
-            'icon-correct': "url('assets/images/icon-correct.svg')",
-            'icon-css': "url('assets/images/icon-css.svg')",
-            'icon-error': "url('assets/images/icon-error.svg')",
-            'icon-html': "url('assets/images/icon-html.svg')",
-            'icon-incorrect': "url('assets/images/icon-incorrect.svg')",
-            'icon-js': "url('assets/images/icon-js.svg')",
-            'icon-moon-dark': "url('assets/images/icon-moon-dark.svg')",
-            'icon-moon-light': "url('assets/images/icon-moon-light.svg')",
-            'icon-sun-dark': "url('assets/images/icon-sun-dark.svg')",
-            'icon-sun-light': "url('assets/images/icon-sun-light.svg')",
-            'pattern-background-desktop-dark': "url('assets/images/pattern-background-desktop-dark.svg')",
-            'pattern-background-desktop-light': "url('assets/images/pattern-background-desktop-light.svg')",
-            'pattern-background-mobile-dark': "url('assets/images/pattern-background-mobile-dark.svg')",
-            'pattern-background-mobile-light': "url('assets/images/pattern-background-mobile-light.svg')",
-            'pattern-background-tablet-dark': "url('assets/images/pattern-background-tablet-dark.svg')",
-            'pattern-background-tablet-light': "url('assets/images/pattern-background-tablet-light.svg')",
-        },
-
         extend: {
             fontFamily: {
-                sans: ['Rubik','Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ['Rubik', 'Inter var', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
                 'display': ['144px', {
@@ -61,8 +41,29 @@ module.exports = {
                 'desktop': '1440px',
                 'tablet': '768px',
                 'mobile': '375px'
+            },
+            backgroundImage: {
+                'icon-accessibility': "url('/public/assets/images/icon-accessibility.svg')",
+                'icon-correct': "url('/public/assets/images/icon-correct.svg')",
+                'icon-css': "url('/public/assets/images/icon-css.svg')",
+                'icon-error': "url('/public/assets/images/icon-error.svg')",
+                'icon-html': "url('/public/assets/images/icon-html.svg')",
+                'icon-incorrect': "url('/public/assets/images/icon-incorrect.svg')",
+                'icon-js': "url('/public/assets/images/icon-js.svg')",
+                'icon-moon-light': "url('/public/assets/images/icon-moon-light.svg')",
+                'icon-moon-dark': "url('/public/assets/images/icon-moon-dark.svg')",
+                'icon-sun-light': "url('/public/assets/images/icon-sun-light.svg')",
+                'icon-sun-dark': "url('/public/assets/images/icon-sun-dark.svg')",
+                'pattern-background-desktop-light': "url('/public/assets/images/pattern-background-desktop-light.svg')",
+                'pattern-background-mobile-light': "url('/public/assets/images/pattern-background-mobile-light.svg')",
+                'pattern-background-tablet-light': "url('/public/assets/images/pattern-background-tablet-light.svg')",
+                'pattern-background-tablet-dark': "url('/public/assets/images/pattern-background-tablet-dark.svg')",
+                'pattern-background-desktop-dark': "url('/public/assets/images/pattern-background-desktop-dark.svg')",
+                'pattern-background-mobile-dark': "url('/public/assets/images/pattern-background-mobile-dark.svg')",
+
             }
         },
+
         colors: {
             'purple': '#A729F5',
             'dark-navy': '#313E51',
