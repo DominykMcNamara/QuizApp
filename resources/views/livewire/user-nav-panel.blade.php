297d77214ebf90d8fe274b4cfaed530bcb1c1cc9
DominykMcNamara <dominyksmith@gmail.com>
2024-04-29 21:56:12 -0400

    Add an Alpine dropdown to the user profile photo

--- resources/views/livewire/user-nav-panel.blade.php
+++ resources/views/livewire/user-nav-panel.blade.php
@@ -1,16 +1,19 @@
 <div class="flex items-center gap-2">
-    <a href="{{ route('logout') }}"
-       class="bg-purple hover:opacity-80 text-white text-center py-2 px-4 rounded-md"
-       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-    >
-        Logout
-    </a>
-
+    <p class="text-purple font-bold">{{ $user->username }}</p>
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         @csrf
     </form>
-    <img src="storage/{{ $user->photo }}" alt="{{ $user->username }}" class="w-12 h-12
-        rounded-full" >
-    <p class="text-purple font-bold">{{ $user->username }}</p>
+    <div x-data="{ open: false }">
+    <img x-on:click="open = !open" src="storage/{{ $user->photo }}" alt="{{ $user->username }}" class="w-12 h-12
+        rounded-full cursor-pointer" >
+        <div x-show="open" class="absolute w-32 h-32 ">
+            <a href="{{ route('logout') }}"
+               class="  text-center hover:text-light-bluish hover:underline "
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+            >
+                Logout
+            </a>
+        </div>
+    </div>
 </div>
 
